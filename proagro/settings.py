dd2be742c907ad75d429144ce6ef15a22ea33c6a
iasmim <iasmimstuff@gmail.com>
2022-09-04 19:37:31 -0300

    feat: heroku deploy

--- proagro/settings.py
+++ proagro/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['api-proagro.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['api-proagro.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
